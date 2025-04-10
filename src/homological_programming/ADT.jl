6a8bf11fc9ed516af4aef5352f5c1f0e4872d14d
Christian Scaff <97995580+cscaff@users.noreply.github.com>
2025-04-10 14:30:43 -0400

    Reordered file to run top-bottom.

--- src/homological_programming/ADT.jl
+++ src/homological_programming/ADT.jl
@@ -25,65 +25,12 @@ where A, B, C represent restriction maps and x, y, and z represent vertex stalks
 a shared edge stalk.
 
 """
-module CellularSheafTerm
+module CellularSheafAST
 
 using MLStyle
 using StructTypes
 
 abstract type AbstractTerm end
-
-
-@doc """ CellularSheafTerm
-
-A cellular sheaf term represents the root node in our AST. It contains two child nodes:
-- Context (A list of judgements)
-- Equations (A list of equations)
-
-"""
-CellularSheafTerm
-
-@data CellularSheafTerm <: AbstractTerm begin
-    SheafExpr(context::Vector{Judgement}, equations::Vector{Equation})
-end
-
-StructTypes.StructType(::Type{CellularSheafTerm}) = StructTypes.AbstractType()
-StructTypes.subtypekey(::Type{CellularSheafTerm}) = :_type
-StructTypes.subtypes(::Type{UWDTerm}) = (SheafExpr=SheafExpr)
-
-@doc """ Judgement
-
-A judgement is a child node of a context node. It represents a variable declaration in our language.
-A declaration can be typed "A::map" or untyped "A" in the situation we are already passed a restriction
-map that is inferred.
-
-"""
-Judgement
-
-@data Judgement <: AbstractTerm begin
-    untyped_var(name::Symbol)
-    typed_var(name::Symbol, type::TypeName)  
-end
-
-StructTypes.StructType(::Type{Judgement}) = StructTypes.AbstractType()
-StructTypes.subtypekey(::Type{Judgement}) = :_type
-StructTypes.subtypes(::Type{Judgement}) = (untyped_var=untyped_var, typed_var=typed_var)
-
-@doc """ Equation
-
-An equation is a child node of CellularSheafTerm, the root node. It contains two products:
-For instance, "A*x" and "B*y" which represents the restriction map and vertex stalks.
-They are related through an equality operator "=".
-
-"""
-Equation
-
-@data Equation <: AbstractTerm begin
-    Eq(lhs::Product, rhs::Product)
-end
-
-StructTypes.StructType(::Type{Equation}) = StructTypes.AbstractType()
-StructTypes.subtypekey(::Type{Equation}) = :_type
-StructTypes.subtypes(::Type{Equation}) = (Equation=Equation)
 
 @docs """ Product
 
@@ -119,4 +66,56 @@ StructTypes.StructType(::Type{TypeName}) = StructTypes.AbstractType()
 StructTypes.subtypekey(::Type{TypeName}) = :_type
 StructTypes.subtypes(::Type{TypeName}) = (TypeName=TypeName)
 
+@doc """ Equation
+
+An equation is a child node of CellularSheafTerm, the root node. It contains two products:
+For instance, "A*x" and "B*y" which represents the restriction map and vertex stalks.
+They are related through an equality operator "=".
+
+"""
+Equation
+
+@data Equation <: AbstractTerm begin
+    Eq(lhs::Product, rhs::Product)
 end
+
+StructTypes.StructType(::Type{Equation}) = StructTypes.AbstractType()
+StructTypes.subtypekey(::Type{Equation}) = :_type
+StructTypes.subtypes(::Type{Equation}) = (Equation=Equation)
+
+end
+
+@doc """ Judgement
+
+A judgement is a child node of a context node. It represents a variable declaration in our language.
+A declaration can be typed "A::map" or untyped "A" in the situation we are already passed a restriction
+map that is inferred.
+
+"""
+Judgement
+
+@data Judgement <: AbstractTerm begin
+    untyped_var(name::Symbol)
+    typed_var(name::Symbol, type::TypeName)  
+end
+
+StructTypes.StructType(::Type{Judgement}) = StructTypes.AbstractType()
+StructTypes.subtypekey(::Type{Judgement}) = :_type
+StructTypes.subtypes(::Type{Judgement}) = (untyped_var=untyped_var, typed_var=typed_var)
+
+@doc """ CellularSheafTerm
+
+A cellular sheaf term represents the root node in our AST. It contains two child nodes:
+- Context (A list of judgements)
+- Equations (A list of equations)
+
+"""
+CellularSheafTerm
+
+@data CellularSheafTerm <: AbstractTerm begin
+    SheafExpr(context::Vector{Judgement}, equations::Vector{Equation})
+end
+
+StructTypes.StructType(::Type{CellularSheafTerm}) = StructTypes.AbstractType()
+StructTypes.subtypekey(::Type{CellularSheafTerm}) = :_type
+StructTypes.subtypes(::Type{UWDTerm}) = (SheafExpr=SheafExpr)
